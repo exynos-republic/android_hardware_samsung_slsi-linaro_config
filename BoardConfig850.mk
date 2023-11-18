7b1c7676d4cfc6999eb3e7cc557eca3bebbd05ea
K9100ii <k9100ii.ki@gmail.com>
2023-11-18 18:05:50 +0000

    config: exynos850: Import new 13 BSP flags
    and switch to gralloc 4. I'm not so sure if there are any 850 devices
    using 3, but until I find out there is one, if any, this can do fine.
    
    Codec2 flags, also present in Linaro E850 device trees, will be for later.
    
    Change-Id: I0b9b1a16df2cfc999d29f0aa326897e5578efa2c

--- BoardConfig850.mk
+++ BoardConfig850.mk
@@ -19,9 +19,12 @@ TARGET_SOC_BASE := exynos3830
 USE_OPENGL_RENDERER := true
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 BOARD_USES_EXYNOS5_COMMON_GRALLOC := true
-BOARD_USES_EXYNOS_GRALLOC_VERSION := 3
+BOARD_USES_EXYNOS_GRALLOC_VERSION := 4
 BOARD_USES_ALIGN_RESTRICTION := true
 BOARD_EXYNOS_S10B_FORMAT_ALIGN := 64
+BOARD_USES_GRALLOC_ION_SYNC := true
+BOARD_USES_EXYNOS_DATASPACE_FEATURE := true
+BOARD_USES_GRALLOC_SCALER_WFD := true
 
 # Samsung OpenMAX Video
 BOARD_USE_DMA_BUF := true
@@ -39,6 +42,7 @@ BOARD_USE_CUSTOM_COMPONENT_SUPPORT := true
 BOARD_USE_VIDEO_EXT_FOR_WFD_HDCP := true
 BOARD_USE_SINGLE_PLANE_IN_DRM := true
 BOARD_USE_WA_ION_BUF_REF := true
+BOARD_USE_SMALL_SECURE_MEMORY := true
 
 # HWComposer
 BOARD_HWC_VERSION := hwc3
@@ -54,6 +58,12 @@ BOARD_USES_HWC_SERVICES := false
 VSYNC_EVENT_PHASE_OFFSET_NS := 0
 SF_VSYNC_EVENT_PHASE_OFFSET_NS := 0
 
+# WifiDisplay
+BOARD_USES_VIRTUAL_DISPLAY := true
+BOARD_USES_DISABLE_COMPOSITIONTYPE_GLES := true
+BOARD_USES_SECURE_ENCODER_ONLY := true
+BOARD_USES_CONTIG_MEMORY_FOR_SCRATCH_BUF := true
+
 # SCALER
 BOARD_USES_DEFAULT_CSC_HW_SCALER := true
 BOARD_DEFAULT_CSC_HW_SCALER := 4
@@ -62,9 +72,26 @@ BOARD_HAS_SCALER_ALIGN_RESTRICTION := true
 
 # LIBHWJPEG
 TARGET_USES_UNIVERSAL_LIBHWJPEG := true
-BOARD_HWJPEG_ANDROID_VERSION := 12
 
 # Acryl
 BOARD_LIBACRYL_DEFAULT_SCALER := mscl_3830
+
+# Gralloc4
+SOONG_CONFIG_exynosgraphicbuffer_gralloc_version := four
+SOONG_CONFIG_NAMESPACES += arm_gralloc
+SOONG_CONFIG_arm_gralloc := \
+        gralloc_arm_no_external_afbc \
+        mali_gpu_support_afbc_basic \
+        gralloc_init_afbc \
+        gralloc_ion_sync_on_lock \
+        gralloc_scaler_wfd \
+        gralloc_product_vendor_version \
+
+SOONG_CONFIG_arm_gralloc_gralloc_arm_no_external_afbc := true
+SOONG_CONFIG_arm_gralloc_mali_gpu_support_afbc_basic := false
+SOONG_CONFIG_arm_gralloc_gralloc_init_afbc := true
+SOONG_CONFIG_arm_gralloc_gralloc_ion_sync_on_lock := $(BOARD_USES_GRALLOC_ION_SYNC)
+SOONG_CONFIG_arm_gralloc_gralloc_scaler_wfd := $(BOARD_USES_GRALLOC_SCALER_WFD)
+SOONG_CONFIG_arm_gralloc_gralloc_product_vendor_version := true
 
 include hardware/samsung_slsi-linaro/config/BoardConfigCommon.mk
